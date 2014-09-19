aecc7f15300f536781d89995161ab05e35c6d90c
Noam Ross <noam.ross@gmail.com>
2014-09-19 11:14:54 -0700

    updating

--- inst/manuscript.md
+++ inst/manuscript.md
@@ -1,6 +1,9 @@
-% Progress of an Epidemic under Age Structure and Repeated Infections
-% Noam Ross
-% 13-12-04 10:56:32
+---
+author:
+- Noam Ross
+date: '13-12-04 10:56:32'
+title: Progress of an Epidemic under Age Structure and Repeated Infections
+...
 
 Abstract
 ========
@@ -20,21 +23,123 @@ Introduction
 -   Review of overlap of the two (only a couple of papers)
     -   [@Pugliese2004; @Pugliese2008]
 -   Diseases modeled as SI for which multiple infection may be appropriate.
-    - Sudden Oak Death
-    - What else? Chestnut blight, 
-    
+    -   Sudden Oak Death
+    -   What else? Chestnut blight,
 
-The transient phase is often of importance, especially in the case of newly invading diseases.
+The transient phase is often of importance, especially in the case of newly
+invading diseases.
 
-@Adler1992 showed that the third moment of the distribution was a function of the mean and mean/variance ratio, but only *asympotically*
-## Models
+@Adler1992 showed that the third moment of the distribution was a function of
+the mean and mean/variance ratio, but only *asymptotically*
+
+Models
+------
+
+I created a model representing a stage-structured population invaded by a
+macroparasite-like disease. The population is divided into classes by stage
+$(i)$ and number of infections $j$. New individuals enter the smallest
+disease-free, stage $(i = 1, j = 0)$, via density-dependent recruitment function
+$F(\cdot)$:
+
+$$F(\cdot) = \sum_{i,j} f_i N_i 
+            \left( 1 - \frac{\sum_{i,j} \omega_i N_i}{K}\right)$$
+
+Individuals advance to larger stages at constant rates $g_i$. Each stage has
+specific fecundity $f_i$ and disease-independent mortality $(d_i)$ rates, and
+contribution to density dependent $\omega_i$, reflecting the increased shading
+of larger trees.
+
+Infection in the model is density-dependent. Each infection on each individual
+produces spores at rate $\lambda_i$ resulting in a total spore burden
+$(\Lambda)$ of
+
+$$\sum_i \lambda_i \sum_j j N_{i,h}$$
+
+Individuals become infected, advancing from $j$ to $j + 1$ at rate
+$\beta_i \Lambda$. Individual infections may recover at rate $\mu_i$.
+
+Trees die of disease at a rate proportional to their number of infections
+determined stage-specific parameter $\alpha_i$, for a total mortality rate of
+$d_i + j \alpha_i$.
+
+In the first version of the model, demographic and epidemiological rates were
+set to constant values across stages.
+
+The model is represented by the following infinite set of equations:
+
+Equations \$\$
+
+While the number of infection classes $j$ is infinite, for purposes of
+simulation we limited the number of infection classes to $j_{\max} = 150$.
+Individuals with the maximum number of infections $(j = j_\max)$ were assumed to
+have zero vulnerability $(\lambda)$ to new infections.
+
+By setting the maximum number of infections per individual $(j_{\max})$ to one,
+this is converted to a stage-structured $SI$ disease model where $N = S + I$ and
+$I = P$.
+
+The multiple-infection model and the $SI$ model represent
+
+In the first comparison, I increased the disease-mortality rate $(\alpha)$ in
+the $SI$ model so as to produce identical overall mortality rates of infected
+trees \$(j \geq 1) at equilibrium.
+
+In the second comparison, I modified age-specific mortality rates in the $SI$
+model so as to match the equilibrium mortality rates of infected trees in each
+stage class in the multiple-infection model.
+
+Equilibrium analysis
+--------------------
+
+At identical equilibrium mortality rates for infected individuals $\alpha_{SI}$
+model is related to \$\alpha\_{\text{multi}} by
+
+Here I assume the negative binomial distribution in calculating equilibrium
+rates.
+
+At equilibrium, individuals in later stages have considerably higher mortality
+rates. To produce this activity in the $SI$ model, I modify the $\alpha_i$
+vector.
+
+Transient dynamics
+------------------
+
+@Anderson1978 approximate this infinite system by assuming a negative-binomial
+distribution of the number of infections across individuals. However, this
+approximation breaks down in the case of discrete stage structure (shown in
+Appendix I). Also, @Adler1992 demonstrated that the distribution of infections
+only approached a stable form at equilibrium. Thus simulation of the infinite
+system is necessary for analysis of transient dynamics.
+
+In Figure X, I show the Klieber-Luwdig (\#SP) divergence (KLD) (\#REF) of the
+distribution of infections relative to a best-fit negative-binomial
+approximation determined by maximum likelihood. Over the course of the epidemic
+the distribution of infections, both within and across stage classes, diverges
+wildly from a negative-binomial distribution. As the system approaches
+equilibrium, the distribution's form comes to resemble the negative-binomial,
+but it does not approach this.
+
+Figures Xa-c demonstrate why this occurs by showing the full distribution of
+infections for each stage at several snapshots in time. Individuals infected
+early in the epidemic enter larger stage classes carrying infections, leading to
+high mean infections in these older classes. Shortly after, the pulse of
+mortality following the epidemic results in new recruitment (due to density
+dependence), and many uninfected individuals entering the smallest size class.
+Overall, this results in a bimodal distribution of infections. Later in the
+course of the epidemic, the distribution become unimodal. Even at equilibrium,
+though, the the regular influx of new, uninfected individuals, which is balanced
+by mortality, means the system never approaches a negative binomial
+distribution.
+
+[The online version of this article contains an interactive graphic of this]
 
 -   A version of @Anderson1978 ODEs with age structure
 -   An infinite set of ODEs with age structure
 -   An equivalent stochastic poisson-process based model with multiple
     infections and age structure
-    
-## Results
+
+Results
+-------
 
 -   Analytics: Show how the @Anderson1978 deviates from the infinite set of
     equations when there is age structure
@@ -44,40 +149,58 @@ The transient phase is often of importance, especially in the case of newly inva
 -   Compare each ODE model with outputs of stochastic model. How do each compare
     to steady-state and transient conditions?
 
-## Discussion
+Discussion
+----------
 
 -   Underlying assumptions of infection/parasite distribution break down in the
     face of other population structure
 -   Disease structure can reflect underlying pop structure
 -   What diseases should be represented with multiple infections? What disease
     have age-related patterns that may be due to repreated infections?
-    
-    
-## General Notes
+
+General Notes
+-------------
 
 -   Bimodal distributions occur even in the absence of age structure. They occur
     In the transient due to the arrival of new individuals in the space occupied
-    by the recently killed invididuals.  Total behavior in the face of age
-    divisions with no demographic differences in age strucutre is identical
--   Neg. binomial always problematic with new births occurring.  Real
-    distibution as more zero and small values than predicted with negative
+    by the recently killed invididuals. Total behavior in the face of age
+    divisions with no demographic differences in age structure is identical
+-   Neg. binomial always problematic with new births occurring. Real distibution
+    has more zero and small values than predicted with negative binomial.
+    However, it is a much bigger problem during the transient period of the
+    disease. (As was previously discussed by XXXX). As the disease approaches a
+    long-term equilibrium, the distribution is close but not quite negative
     binomial.
+
 -   Diseased equilibrium always has lower N, and smaller-individual skewed stage
     distribution than DFE.
 -   Relative mortality rates diverge during invasion, then converge a little.
-    The degree of shrinkage is greater with low growth rates; with high growth
-    rates, mortality rates don't spread out or bounce back as much.
+    Divergence occurs in stages as infected individuals enter each successive
+    age class, increasing its mortality rate. The degree of shrinkage is greater
+    with low growth rates; with high growth rates, mortality rates don't spread
+    out or bounce back as much.
 -   Equilbrium mortality rates aren't that different across growth rates.
 -   In slow-growing conditions, mortality rate diverge later, because at first
-    very little of the mortality in older plants is from infections while
-    they are younger. 
+    very little of the mortality in older plants is from infections while they
+    are younger.  
 -   Also, in slow-growing conditions, mortality in each group will be higher,
     though since more individuals will be small, overall mortality remains the
     same across growth rates.
-    
-    
-
 
 Key points: variability in infectious period
 
-Management implications: Remove the *most* infected trees 
+Management implications: Remove the *most* infected trees
+
+If the ability to cull infected individuals is limited, the epidemic is likely
+to be better contained by removing the *most* infected trees.
+
+One challenge is to distinguish between number of infections and disease stage.
+These are likely to be very challenging to disentangle in the field, as
+late-stage individuals also likely will have been infected multiple times, as
+well.
+
+@Cobb2012 found that mortality rates in infected trees were infected, suggesting
+that mean
+
+Possible other systems: Chytrid fungus, white nose syndrome.
+
--- master-equation-numerics.R
+++ master-equation-numerics.R
@@ -23,7 +23,7 @@ parms = list(
   xi = c(1, 1),
   omega = c(1,1),
   K = 10,
-  progress = 1,
+  progress = 0,
   times.min = 0,
   times.max = 2,
   times.by = 0.01,
@@ -73,39 +73,520 @@ parms2 = list(
   infect_vector = c(0.0001)
 )
 
+parms4 = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(15, 15, 15, 15),
+  g = c(1, 1, 1, 0),
+  d = c(0.2, 0.2, 0.2, 0.2),
+  alpha = c(1,1,1,1),
+  lamda = c(3, 3,3, 3),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 2,
+  times.by = 0.01,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms41 = list(
+  n_stages = 4,
+  n_parasites = 1,
+  f = c(15, 15, 15, 15),
+  g = c(1, 1, 1, 0),
+  d = c(0.2, 0.2, 0.2, 0.2),
+  alpha = c(1,1,1,1),
+  lamda = c(3, 3,3, 3),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 2,
+  times.by = 0.01,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4s = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(15, 15, 15, 15),
+  g = c(0.75, 0.75, 0.75, 0),
+  d = c(0.2, 0.2, 0.2, 0.2),
+  alpha = c(1,1,1,1),
+  lamda = c(3, 3,3, 3),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 2,
+  times.by = 0.01,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(0.1, 0.1, 0.1, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(1,1,1,1),
+  lamda = c(3, 3,3, 3),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 10,
+  times.by = 0.01,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf2 = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(1, 1, 1, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(1,1,1,1),
+  lamda = c(3, 3,3, 3),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 10,
+  times.by = 0.01,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf3 = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(0.01, 0.01, 0.01, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(1,1,1,1),
+  lamda = c(3, 3,3, 3),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 10,
+  times.by = 0.01,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf4 = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(10, 10, 10, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(1,1,1,1),
+  lamda = c(3, 3,3, 3),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 10,
+  times.by = 0.01,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(0.1, 0.1, 0.1, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(0.2,0.2,0.2,0.2),
+  lamda = c(1, 1,1, 1),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 25,
+  times.by = 0.02,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf2 = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(1, 1, 1, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(0.2,0.2,0.2,0.2),
+  lamda = c(1, 1,1, 1),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 25,
+  times.by = 0.02,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf3 = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(0.01, 0.01, 0.01, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(0.2,0.2,0.2,0.2),
+  lamda = c(1, 1,1, 1),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 25,
+  times.by = 0.02,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+parms4sf4 = list(
+  n_stages = 4,
+  n_parasites = 150,
+  f = c(1.5, 1.5, 1.5, 1.5),
+  g = c(10, 10, 10, 0),
+  d = c(0.02, 0.02, 0.02, 0.02),
+  alpha = c(0.2,0.2,0.2,0.2),
+  lamda = c(1, 1,1, 1),
+  Beta = c(1, 1, 1, 1),
+  mu = c(1, 1, 1, 1),
+  xi = c(1, 1, 1, 1),
+  omega = c(1,1, 1, 1),
+  K = 10,
+  progress = 1,
+  times.min = 0,
+  times.max = 25,
+  times.by = 0.02,
+  inits = 0,
+  infect_vector = c(0, 0.0001, 0, 0)
+  )
+
+
+
 require(deSolve)
 
-out = run_sodp(parms)
-d = process_sodp(out)
-d = sodp_totals(d)
-stats = sodp_stats(d)
-stats.m = melt(stats, id.vars=c("time", "Species", "SizeClass"))
+# out = run_sodp(parms)
+# d = process_sodp(out)
+# d = sodp_totals(d)
+# stats = sodp_stats(d)
+# stats.m = melt(stats, id.vars=c("time", "Species", "SizeClass"))
+# 
+# out1 = run_sodp(parms1)
+# d1 = process_sodp(out1)
+# d1 = sodp_totals(d1)
+# stats1 = sodp_stats(d1)
+# stats.m1 = melt(stats1, id.vars=c("time", "Species", "SizeClass"))
+# 
+# 
+# 
+# out2 = run_sodp(parms2)
+# d2 = process_sodp(out2)
+# d2 = sodp_totals(d2)
+# stats2 = sodp_stats(d2)
+# stats.m2 = melt(stats2, id.vars=c("time", "Species", "SizeClass"))
+# 
+# out4 = run_sodp(parms4)
+# d4 = process_sodp(out4)
+# d4 = sodp_totals(d4)
+# stats4 = sodp_stats(d4)
+# stats.m4 = melt(stats4, id.vars=c("time", "Species", "SizeClass"))
+# 
+# out41 = run_sodp(parms41)
+# d41 = process_sodp(out41)
+# d41 = sodp_totals(d41)
+# stats41 = sodp_stats(d41)
+# stats.m41 = melt(stats41, id.vars=c("time", "Species", "SizeClass"))
+# 
+# out4s = run_sodp(parms4s)
+# d4s = process_sodp(out4s)
+# d4s = sodp_totals(d4s)
+# stats4s = sodp_stats(d4s)
+# stats.m4s = melt(stats4s, id.vars=c("time", "Species", "SizeClass"))
+# 
+out4sf = run_sodp(parms4sf)
+d4sf = process_sodp(out4sf)
+d4sf = sodp_totals(d4sf)
+stats4sf = sodp_stats(d4sf)
+stats.m4sf = melt(stats4sf, id.vars=c("time", "Species", "SizeClass"))
 
-out1 = run_sodp(parms1)
-d1 = process_sodp(out1)
-d1 = sodp_totals(d1)
-stats1 = sodp_stats(d1)
-stats.m1 = melt(stats1, id.vars=c("time", "Species", "SizeClass"))
+out4sf2 = run_sodp(parms4sf2)
+d4sf2 = process_sodp(out4sf2)
+d4sf2 = sodp_totals(d4sf2)
+stats4sf2 = sodp_stats(d4sf2)
+stats.m4sf2 = melt(stats4sf2, id.vars=c("time", "Species", "SizeClass"))
 
+out4sf3 = run_sodp(parms4sf3)
+d4sf3 = process_sodp(out4sf3)
+d4sf3 = sodp_totals(d4sf3)
+stats4sf3 = sodp_stats(d4sf3)
+stats.m4sf3 = melt(stats4sf3, id.vars=c("time", "Species", "SizeClass"))
 
+out4sf4 = run_sodp(parms4sf4)
+d4sf4 = process_sodp(out4sf4)
+d4sf4 = sodp_totals(d4sf4)
+stats4sf4 = sodp_stats(d4sf4)
+stats.m4sf4 = melt(stats4sf4, id.vars=c("time", "Species", "SizeClass"))
+ 
+# 
+# ggplot(stats.m4, aes(x=time, y=value, col=SizeClass)) + 
+#   facet_wrap(~variable, scales="free_y") +
+#   geom_line() 
+# 
+# ggplot(stats.m41, aes(x=time, y=value, col=SizeClass)) + 
+#   facet_wrap(~variable, scales="free_y") +
+#   geom_line() 
+# 
+# ggplot(stats.m4s, aes(x=time, y=value, col=SizeClass)) + 
+#   facet_wrap(~variable, scales="free_y") +
+#   geom_line() 
 
-out2 = run_sodp(parms2)
-d2 = process_sodp(out2)
-d2 = sodp_totals(d2)
-stats2 = sodp_stats(d2)
-stats.m2 = melt(stats2, id.vars=c("time", "Species", "SizeClass"))
+plots = list()
+x = 1
+for(datt in list(stats.m4sf3, stats.m4sf, stats.m4sf2, stats.m4sf4)) {
 
+datt = subset(datt, time < 25 & variable=="RMortInfRate" &SizeClass != "Total")  
+datt$SizeClass = factor(datt$SizeClass, levels = c("4","3","2","1"))
+plots[[x]] = ggplot(datt, aes(x=time, y=value, col=SizeClass)) + 
+  geom_line(size=1) +
+  ylab("Relative mortality rate of infected trees") +
+  xlab("Time") +
+  ylim(1,5.75) +
+  scale_color_manual(name="Tree Size Class", breaks=c("1","2","3","4"), values=brewer.pal(6, "Blues")[3:6]) +
+  theme_nr +
+   theme(text=element_text(family="Lato"),
+        legend.position = c(0.8, 0.7),
+        legend.text = element_text(size=16),
+        legend.title = element_text(size=18),
+        axis.title.y=element_text(vjust=1),
+        axis.title.x=element_text(vjust=-.5),
+        axis.text=element_text(size=16),
+        legend.key.size=unit(1.2, "cm")
+        )
+x = x+1
+}
 
+plots[[1]]
+plots[[2]]
+plots[[3]]
+plots[[4]]
 
-ggplot(stats.m, aes(x=time, y=value, col=SizeClass)) + 
-  facet_wrap(~variable, scales="free_y") +
-  geom_line() 
-
-ggplot(stats.m1, aes(x=time, y=value, col=SizeClass)) + 
-  facet_wrap(~variable, scales="free_y") +
-  geom_line() 
+ggsave("~/Dropbox/Workspace/dyn2.png", plots[[2]], width = 10, height=7)
+ggsave("~/Dropbox/Workspace/dyn4.png", plots[[4]], width = 10, height=7)
 
 require(manipulate)
+
+manipulate(ggplot(d4sf2[abs(time-TIME) < 1e-5 & SizeClass != "Total",], aes(x=Infected, y = Population, fill=SizeClass)) +
+  geom_area(position="identity", alpha=0.5) + xlim(0,50),
+      TIME = slider(min=min(d4sf4$time), max=max(d4sf4$time), 
+                       step=0.02,
+                       label="Time Step"))
+library(dplyr)
+library(RColorBrewer)
+TIME = 1.9
+d4sf2mm = d4sf2 %>% subset(abs(time-TIME) < 1e-5 & SizeClass != "Total") %>% 
+  group_by(SizeClass) %>%
+  summarize(maxpop=max(Population)) %>%
+  arrange(-maxpop) %>% select(SizeClass) %>% as.data.frame
+d4sf2mm = as.character(d4sf2mm$SizeClass)
+
+
+
+
+TIME = 1.9
+  
+d4sf2$SizeClass = factor(d4sf2$SizeClass, levels = c(d4sf2mm, "Total"))
+distplot1 = ggplot(d4sf2[(abs(time-TIME) < 1e-5) & SizeClass != "Total" & Population > 1e-5,],
+       aes(x=Infected, y = Population, fill=SizeClass)) +
+  geom_area(position="identity") +
+  geom_area(color="black",position="identity", alpha=0.9, lwd=0.25, show_guide=FALSE) +
+  xlim(0,50) + 
+  theme_nr +
+  scale_fill_manual(name = "Tree Size Class", breaks=c("1", "2", "3", "4"),
+                    values = brewer.pal(4, "Blues")[c(1,4,3,2)]) +
+  xlab("Number of Infections") +
+  ylab("Population in Infection Class") +
+  theme_nr +
+  theme(text=element_text(family="Lato"),
+        strip.text=element_text(size=16),
+        strip.background=element_blank(),
+        legend.position = c(0.8, 0.83),
+        legend.text = element_text(size=16),
+        legend.title = element_text(size=18),
+        axis.title.y=element_text(vjust=1),
+        axis.title.x=element_text(vjust=-.5),
+        axis.text=element_text(size=16),
+        panel.margin = unit(0, "cm"),
+        legend.key.size=unit(1.2, "cm")
+        )
+distplot1
+ggsave("~/Dropbox/Workspace/distplot1.png", distplot1, width = 10, height=7)
+
+
+TIME = 2.3
+  
+d4sf2$SizeClass = factor(d4sf2$SizeClass, levels = c(d4sf2mm, "Total"))
+distplot2 = ggplot(d4sf2[(abs(time-TIME) < 1e-5) & SizeClass != "Total" & Population > 1e-5,],
+       aes(x=Infected, y = Population, fill=SizeClass)) +
+  geom_area(position="identity") +
+  geom_area(color="black",position="identity", alpha=0.9, lwd=0.25, show_guide=FALSE) +
+  xlim(0,50) + 
+  theme_nr +
+  scale_fill_manual(name = "Tree Size Class", breaks=c("1", "2", "3", "4"),
+                    values = brewer.pal(4, "Blues")[c(1,4,3,2)]) +
+  xlab("Number of Infections") +
+  ylab("Population in Infection Class") +
+  theme_nr +
+  theme(text=element_text(family="Lato"),
+        strip.text=element_text(size=16),
+        strip.background=element_blank(),
+        legend.position = c(0.8, 0.83),
+        legend.text = element_text(size=16),
+        legend.title = element_text(size=18),
+        axis.title.y=element_text(vjust=1),
+        axis.title.x=element_text(vjust=-.5),
+        axis.text=element_text(size=16),
+        panel.margin = unit(0, "cm"),
+        legend.key.size=unit(1.2, "cm")
+        )
+
+distplot2
+ggsave("~/Dropbox/Workspace/distplot2.png", distplot2, width = 10, height=7)
+
+TIME = 25
+
+d4sf2mm = d4sf2 %>% subset(abs(time-TIME) < 1e-5 & SizeClass != "Total") %>% 
+  group_by(SizeClass) %>%
+  summarize(maxpop=max(Population)) %>%
+  arrange(-maxpop) %>% select(SizeClass) %>% as.data.frame
+d4sf2mm = as.character(d4sf2mm$SizeClass)
+  
+d4sf2$SizeClass = factor(d4sf2$SizeClass, levels = c(d4sf2mm, "Total"))
+distplot3 = ggplot(d4sf2[(abs(time-TIME) < 1e-5) & SizeClass != "Total" & Population > 1e-5,],
+       aes(x=Infected, y = Population, fill=SizeClass)) +
+  geom_area(position="identity") +
+  geom_area(color="black",position="identity", alpha=0.9, lwd=0.25, show_guide=FALSE) +
+  xlim(0,50) + 
+  theme_nr +
+  scale_fill_manual(name = "Tree Size Class", breaks=c("1", "2", "3", "4"),
+                    values = rev(brewer.pal(4, "Blues"))) +
+  xlab("Number of Infections") +
+  ylab("Population in Infection Class") +
+  theme_nr +
+  theme(text=element_text(family="Lato"),
+        strip.text=element_text(size=16),
+        strip.background=element_blank(),
+        legend.position = c(0.8, 0.83),
+        legend.text = element_text(size=16),
+        legend.title = element_text(size=18),
+        axis.title.y=element_text(vjust=1),
+        axis.title.x=element_text(vjust=-.5),
+        axis.text=element_text(size=16),
+        panel.margin = unit(0, "cm"),
+        legend.key.size=unit(1.2, "cm")
+        )
+
+distplot3
+ggsave("~/Dropbox/Workspace/distplot3.png", distplot3, width = 10, height=7)
+
+library(scales)
+stats4sf4 = arrange(stats4sf4, time)
+evol1 = ggplot(subset(stats4sf4, SizeClass=="Total"), aes(x=I/N, y = 1/MortInfRate)) +
+  geom_point(size=2.5) +
+  geom_path(size=.75) +
+  theme_nr +
+  ylab("Time to death after infection") +
+  scale_x_continuous(labels = percent, name="Percent Individuals Infected") +
+  theme(text=element_text(family="Lato"),
+        axis.title.y=element_text(vjust=1),
+        axis.title.x=element_text(vjust=-.5),
+        axis.text=element_text(size=16))
+evol1
+ggsave("~/Dropbox/Workspace/evol1.png", evol1, width = 10, height=7)
+
+evol2 = ggplot(subset(stats4sf4, SizeClass=="Total"), aes(x=I, y = 1/MortInfRate)) +
+  geom_point(size=2.5) +
+  geom_path(size=.75) +
+  theme_nr +
+  ylab("Time to death after infection") +
+  scale_x_continuous("Number Individuals Infected") +
+  theme(text=element_text(family="Lato"),
+        axis.title.y=element_text(vjust=1),
+        axis.title.x=element_text(vjust=-.5),
+        axis.text=element_text(size=16))
+evol2
+ggsave("~/Dropbox/Workspace/evol2.png", evol2, width = 10, height=7)
+
+
+
+manipulate(ggplot(droplevels(subset(stats4s, SizeClass != "Total" & abs(time - TIME) < 0.0001)),
+         aes(x = SizeClass, y = MortInfRate)) + geom_bar(stat="identity") +
+          # ylim(0, max(stats4s$MortInfRate, na.rm=TRUE)),
+           ylim(0,30),
+         TIME = slider(min=min(stats4s$time), max=max(stats4$time), 
+                       step=0.02,
+                       label="Time Step")
+)
+
+manipulate(ggplot(droplevels(subset(stats4, SizeClass != "Total" & abs(time - TIME) < 0.0001)),
+         aes(x = SizeClass, y = N)) + geom_bar(stat="identity") +
+           ylim(0, max(stats4$N, na.rm=TRUE)),
+         TIME = slider(min=min(stats4$time), max=max(stats4$time), 
+                       step=0.02,
+                       label="Time Step")
+)
+
+manipulate(ggplot(droplevels(subset(stats41, SizeClass != "Total" & abs(time - TIME) < 0.0001)),
+         aes(x = SizeClass, y = N)) + geom_bar(stat="identity") +
+           ylim(0, max(stats41$N, na.rm=TRUE)),
+         TIME = slider(min=min(stats4$time), max=max(stats4$time), 
+                       step=0.02,
+                       label="Time Step")
+)
+
+
 manipulate(ggplot(subset(SET, variable==VAR & (TOT | SizeClass != "Total")), 
                   aes(x=time, y=value, col=SizeClass)) + 
              geom_line(),
@@ -160,7 +641,7 @@ multstats.m = melt(multstats, id.vars=c("K", "time", "Species", "SizeClass"))
 
 
 ggplot(subset(multstats.m, variable=="MortInfRate"), aes(x=time, y=value, col=SizeClass)) + 
-  facet_wrap(~K, scales="free_y") +
+  facet_wrap(~K, scales="fixed") +
   geom_line() 
 
 manipulate(ggplot(subset(multstats.m, variable==VAR & (TOT | SizeClass != "Total") & (time > TIME)), 
